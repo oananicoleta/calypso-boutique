08eb7fc513d06e5336b249bffb7ae00f137a426f
Oana Clinci <oana.clinci@yahoo.com>
2024-07-31 18:10:43 +0300

    feat: admin page HTML + CSS

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function displayAllProducts() {
       <div class="product-card flex-col gap-20 items-center justify-between">
          <h3 class="card-title">${product.name}</h3>
          <img src=${product.imageUrl} width="200px"/>
-         <p class="card-price">${product.price} lei</p>
+         <p class="card-price">${product.price} RON</p>
+         <a href="../pages/details.html?id=${product.id}">Details</a>
       </div>   
       `
         )
