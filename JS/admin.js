3319cf9244b723af58b12c90ed733b129c433d53
Oana Clinci <oana.clinci@yahoo.com>
2024-08-02 17:06:02 +0300

    feat: admin edit and delete + postman

--- JS/admin.js
+++ JS/admin.js
@@ -10,6 +10,10 @@ function getAllProducts() {
   return fetch(url).then((response) => response.json());
 }
 
+function getProductById(id) {
+  return fetch(`${url}/${id}`).then((response) => response.json());
+}
+
 function displayAllProducts() {
   getAllProducts().then((products) => {
     productsTableBody.innerHTML = products
@@ -22,12 +26,12 @@ function displayAllProducts() {
             <img src="../${product.imageUrl}" />
         </td>
         <td>
-            <button>
+            <button class="edit-${product.id}">
                 <i class="fa-solid fa-pen-to-square"></i>
             </button>
         </td>
                 <td>
-            <button>
+            <button class="delete-${product.id}">
                 <i class="fa-solid fa-trash"></i>
             </button>
         </td>
@@ -46,6 +50,8 @@ const detailsInput = document.getElementById("details");
 const colourInput = document.getElementById("colour");
 const categoryInput = document.getElementById("category");
 const saveProductButton = document.getElementById("save-btn");
+let editMode = false;
+let currentEditableProductId;
 
 saveProductButton.addEventListener("click", saveProduct);
 console.log(
@@ -69,14 +75,50 @@ function saveProduct(event) {
     category: categoryInput.value,
   };
 
-  fetch(url, {
-    method: "POST",
+  fetch(editMode ? `${url}/${currentEditableProductId}` : url, {
+    method: editMode ? "PUT" : "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(product),
   }).then(() => {
     form.reset();
+    displayAllProducts();
+    editMode = false;
+  });
+}
+
+productsTableBody.addEventListener("click", handleActions);
+
+function handleActions(event) {
+  const className = event.target.parentElement.className;
+  if (className.includes("edit")) {
+    const productId = className.split("-")[1];
+    editProduct(productId);
+  } else if (className.includes("delete")) {
+    const productId = className.split("-")[1];
+    deleteProduct(productId);
+  }
+}
+
+function editProduct(id) {
+  getProductById(id).then((product) => {
+    editMode = true;
+    nameInput.value = product.name;
+    priceInput.value = product.price;
+    imageUrlInput.value = product.imageUrl;
+    detailsInput.value = product.details;
+    colourInput.value = product.colour;
+    categoryInput.value = product.category;
+
+    currentEditableProductId = product.id;
+  });
+}
+
+function deleteProduct(id) {
+  fetch(`${url}/${id}`, {
+    method: "DELETE",
+  }).then(() => {
     displayAllProducts();
   });
 }
