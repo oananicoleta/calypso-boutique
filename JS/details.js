855f439efe51f5652caba7535de4bdbdad7239b1
Oana Clinci <oana.clinci@yahoo.com>
2024-08-03 10:09:42 +0300

    feat: details css

--- JS/details.js
+++ JS/details.js
@@ -8,18 +8,26 @@ async function showProductDetails() {
   const response = await fetch(`${url}/${productID}`);
   const product = await response.json();
 
+  console.log("Product fetched:", product);
+
   document.querySelector(".main").innerHTML = `
-    <div>
-    <h2>${product.name}</h2>
-    <img src=${product.imageUrl}/>
-    <p class="card-price">${product.price} RON</p>
-    <select>
-    <option>S</option>
-    <option>M</option>
-    <option>L</option>
-    </select>
-    <a class="buyButton">Add to cart</a>
-    <h4>${product.details}</h4>
+    <div class="details-card flex-col">
+     <h2 class="padding-15">${product.name}</h2>
+     <div class="pic">
+       <img src=${product.imageUrl}/>
+       <p class="card-price padding-15">${product.price} RON</p>
+      </div>
+      <div class="cart-option padding-15">
+       <select>
+         <option>S</option>
+         <option>M</option>
+         <option>L</option>
+       </select>
+       <a class="buyButton">Add to cart</a>
+      </div>
+      <h4 class="padding-15">${product.details}</h4>
     </div>
   `;
+
+  console.log("HTML updated");
 }
